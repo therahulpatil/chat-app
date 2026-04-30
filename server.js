b0729d90ebd2262e0306a8b3d18b7d0f2923c2fc
therahulpatil <patilrahulprafulla@gmail.com>
2026-04-30 20:45:31 +0530

    v0.4

--- server.js
+++ server.js
@@ -1,73 +1,38 @@
 const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-app.use(express.json());
+// Serve frontend
 app.use(express.static("public"));
 
-const SECRET = "secretkey";
+let users = {};
 
-let users = [];
-
-// REGISTER
-app.post("/register", async (req, res) => {
-    const { username, password } = req.body;
-
-    if (!username || !password) {
-        return res.status(400).json({ message: "Enter username & password" });
-    }
-
-    const hashed = await bcrypt.hash(password, 10);
-    users.push({ username, password: hashed });
-
-    res.json({ message: "Registered successfully" });
-});
-
-// LOGIN
-app.post("/login", async (req, res) => {
-    const { username, password } = req.body;
-
-    const user = users.find(u => u.username === username);
-    if (!user) return res.status(400).json({ message: "User not found" });
-
-    const valid = await bcrypt.compare(password, user.password);
-    if (!valid) return res.status(400).json({ message: "Wrong password" });
-
-    const token = jwt.sign({ username }, SECRET);
-
-    res.json({ token, username });
-});
-
-// SOCKET AUTH
-io.use((socket, next) => {
-    const token = socket.handshake.auth?.token;
-
-    if (!token) return next(new Error("No token"));
-
-    try {
-        const decoded = jwt.verify(token, SECRET);
-        socket.username = decoded.username;
-        next();
-    } catch {
-        next(new Error("Invalid token"));
-    }
-});
-
-// SOCKET
+// Socket connection
 io.on("connection", (socket) => {
-    console.log("Connected:", socket.username);
+    console.log("User connected:", socket.id);
 
-    socket.on("chat message", (msg) => {
-        io.emit("chat message", {
-            user: socket.username,
-            message: msg
-        });
+    socket.on("join", (username) => {
+        users[socket.id] = username;
+        io.emit("message", `${username} joined the chat`);
+        io.emit("userList", Object.values(users));
+    });
+
+    socket.on("chatMessage", (msg) => {
+        if (users[socket.id]) {
+            io.emit("message", `${users[socket.id]}: ${msg}`);
+        }
+    });
+
+    socket.on("disconnect", () => {
+        if (users[socket.id]) {
+            io.emit("message", `${users[socket.id]} left the chat`);
+            delete users[socket.id];
+            io.emit("userList", Object.values(users));
+        }
     });
 });
 
